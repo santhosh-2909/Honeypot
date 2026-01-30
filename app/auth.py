0a5858c98ee5e276318d75afdb157f9c9e5f3358
Santhosh <santhoshsakthivel2004@gmail.com>
2026-01-30 15:16:29 +0530

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -1,7 +1,9 @@
+import os
 from fastapi import Header, HTTPException
 
-API_KEY = "YOUR_SECRET_API_KEY"
+API_KEY = os.getenv("HONEY_POT_API_KEY")
 
 def verify_api_key(x_api_key: str = Header(...)):
-    if x_api_key != API_KEY:
+    if not API_KEY or x_api_key != API_KEY:
         raise HTTPException(status_code=401, detail="Invalid API Key")
+    return x_api_key
